4e58902cae9d0aa829e0c211bfdf9e24e19b1990
Вячеслав Островский <slslava007@gmail.com>
2022-02-21 22:05:05 +0200

    Delete summary

--- Lesson5_HomeTask/Program.cs
+++ Lesson5_HomeTask/Program.cs
@@ -3,14 +3,6 @@
     class Program
     {
 
-        /// <summary>
-        /// Note: dividing by 0 can be handled with simple verification if second number is not 0 (if y != 0)​
-        /// Optional task: dividing by 0 can be handled by try/catch construction.​
-
-        /// in case if y = 0, log an error into the file​
-        /// 
-        /// </summary>
-
         static void Main()
         {
             ConsoleController.Start();
